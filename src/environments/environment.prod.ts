397036fd2bc31bf9cfdffa392d9b7968f14449ef
Aayush Bhankale <abhankale@gmail.com>
2020-08-16 16:05:40 +0530

    refactor: enviorment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCsVMRcJc2_4D9pkS371sCzeoJNTy6SQ9U",
+    authDomain: "github-serach.firebaseapp.com",
+    databaseURL: "https://github-serach.firebaseio.com",
+    projectId: "github-serach",
+    storageBucket: "github-serach.appspot.com",
+    messagingSenderId: "888488259051",
+    appId: "1:888488259051:web:cec8a9325382257306bdae",
+    measurementId: "G-WVRXP8W2C7"
+}
 };
